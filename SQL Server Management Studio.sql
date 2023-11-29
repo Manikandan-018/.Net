923d90b5909fd589f7eaabd9921a13342d217884
Manikandan-018 <150133480+Manikandan-018@users.noreply.github.com>
2023-11-29 18:23:44 +0530

    Add files via upload

--- SQL Server Management Studio.sql
+++ SQL Server Management Studio.sql
@@ -1,124 +1,196 @@
 use logicfirst;
 
-create table employee(
-emp_id int primary key,
-ename varchar(25),
-job_desc varchar(20),
-salary decimal(8,2)
+-- Create tables for Hospital Management System
+
+CREATE TABLE patients (
+    patient_id INT PRIMARY KEY,
+    first_name VARCHAR(15),
+    last_name VARCHAR(10),
+    date_of_birth DATE,
+    gender VARCHAR(10),
+    contact_number VARCHAR(15),
+    address VARCHAR(25)
 );
 
-insert into employee values(1,'ram','admin',50000);
-insert into employee values(2,'rish','engineer',40000);
-insert into employee values(3,'brown','doctor',50000);
-insert into employee values(4,'green','Hr',30000);
-insert into employee values(5,'james','Ceo',60000);
-insert into employee values(6,'nath','manager',20000);
-insert into employee values(7,'greek','Ceo',70000);
-insert into employee values(8,'ragu','engineer',10000);
-insert into employee values(9,'roman','contractor',10000);
-insert into employee values(10,'jhon','labour',5000);
-insert into employee values(11,'mike','worker',20000);
+
+INSERT INTO patients (patient_id, first_name, last_name, date_of_birth, gender, contact_number, address)
+VALUES
+(1, 'John', 'Doe', '2006-05-15', 'Male', '1234567890', '123 Main St'),
+(2, 'Jane', 'Smith', '2011-08-20', 'Female', '9876543210', '456 Oak St');
+
+
+SELECT * FROM patients;
+
+
+CREATE TABLE doctors (
+    doctor_id INT PRIMARY KEY,
+    first_name VARCHAR(10),
+    last_name VARCHAR(15),
+    specialization VARCHAR(20),
+    contact_number VARCHAR(10),
+    address VARCHAR(28)
+);
+
+
+INSERT INTO doctors (doctor_id, first_name, last_name, specialization, contact_number, address)
+VALUES
+(1, 'Dr. Michael', 'Johnson', 'Cardiologist', '5551112233', '789 Elm St'),
+(2, 'Dr. Emily', 'Williams', 'Pediatrician', '5554445566', '101 Pine St');
+
+SELECT * FROM doctors;
+
+
+CREATE TABLE appointments (
+    appointment_id INT PRIMARY KEY,
+    patient_id INT,
+    doctor_id INT,
+    appointment_date DATETIME,
+    PRIMARY KEY (patient_id, appointment_date),
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id)
+);
+
+
+INSERT INTO appointments (appointment_id, patient_id, doctor_id, task_description, appointment_date)
+VALUES
+(1, 1, 1, 'Consultation', '2023-04-10 10:00:00');
+
+
+SELECT * FROM appointments;
+
+
+CREATE TABLE medical_records (
+    record_id INT PRIMARY KEY,
+    patient_id INT,
+    doctor_id INT,
+    appointment_id INT,
+    diagnosis TEXT,
+    prescription TEXT,
+    test_results TEXT,
+    PRIMARY KEY (patient_id, appointment_id),
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id),
+    FOREIGN KEY (appointment_id) REFERENCES appointments(appointment_id)
+);
+
+
+SELECT * FROM medical_records;
 
 
 
-select * from employee;
+CREATE TABLE tasks (
+    task_id INT PRIMARY KEY,
+    task_description VARCHAR(25),
+    task_status VARCHAR(50), 
+    assigned_doctor_id INT, 
+    assigned_patient_id INT,
+    assigned_date DATETIME,
+    FOREIGN KEY (assigned_doctor_id) REFERENCES doctors(doctor_id),
+    FOREIGN KEY (assigned_patient_id) REFERENCES patients(patient_id)
+);
 
-select * from employee
-where 'enmae'="roman";
-select * from employee
-where 'enmae'<>"roman";
-
-
-select * from employee
-where job_desc in ("Hr","worker");
-
-select * from emloyee
-where job_desc not in ("worker","doctor");
-
-select * from employee
-where salary between 20000 and 40000;
-
-select * from employee
-limit 7; 
-
--- like 
--- wildcards %(zero or more characters) _(one character)
-
-select * from employee
-where enmae like 'g%';
-
-select * from employee 
-where ename like '_r%';
-
-update employee
-set job_desc="analyst";
-
-select distinct job_desc
-from employee;
-
-select curdate();
+INSERT INTO tasks (task_id, task_description, task_status, assigned_doctor_id, assigned_patient_id, assigned_date)
+VALUES
+(1, 'Blood Test', 'Pending', 1, 1, '2023-04-15 14:00:00');
 
 
-select * from employee
-order by ename;
+
+DELIMITER //
+
+CREATE PROCEDURE GetPatientAppointments(IN p_patient_id INT)
+BEGIN
+  
+    DECLARE patient_first_name VARCHAR(255);
+    DECLARE patient_last_name VARCHAR(255);
+    DECLARE doctor_first_name VARCHAR(255);
+    DECLARE doctor_last_name VARCHAR(255);
+    
+
+    SELECT first_name, last_name INTO patient_first_name, patient_last_name
+    FROM patients
+    WHERE patient_id = p_patient_id;
+
+    SELECT 'Patient Details:', 'Name:', patient_first_name, patient_last_name;
+
+    SELECT appointments.appointment_id,
+           doctors.first_name AS doctor_first_name,
+           doctors.last_name AS doctor_last_name,
+           appointments.task_description,
+           appointments.appointment_date
+    FROM appointments
+    INNER JOIN doctors ON appointments.doctor_id = doctors.doctor_id
+    WHERE appointments.patient_id = p_patient_id;
+
+END //
+
+DELIMITER ;
 
 
-select * from employee
-order by salary;
 
-select * from employee 
-order by salary,ename;
+SELECT * FROM tasks;
 
-select * from employee 
-where job_desc="manager"
-order by salary;
+drop table patient;
 
-select avg (salary)
+drop table doctors;
+
+
+
+delimiter $$
+create procedure employeecnt(
+in jdesc varchar(10)
+)
+begin
+declare total int default 0;
+select count(emp_id)
+into total
 from employee
-where job_desc="ceo";
+where job_desc=jdesc;
+select total;
+end$$
+delimiter ;
 
-select sum(salary)
-from employee
-where job_desc="engineer";
+call employeecnt('ADMIN');
 
-select max(salary)
-from employee;
-
-select min(salary)
-from employee;
-
-select ucase(ename) name,salary
-from employee;
-
-select enmae,concat('RS.',salary)
-from employee;
-
-alter table employee add column Hire_date date;
-
-select * from employee;
- 
- update employee
- set Hire_date="2023-11-25"
- where job_desc="Engineer";
-
-select date_format(curdate(), "%d/%m/%y") as date;
-
-select datediff(curdate(),"2026/05/11") as days; 
-
--- add not null constraint
-alter table employee
-modify ename varchar(30) not null;
-
--- drop not null constarint
-alter table employee
-alter job_desc set default 'unassigned';
+call employeecnt('CEO');
 
 
-drop table employee;
 
-create table branch(
-branch_id int primary key,
-br_name varchar(30) not null,
-addr varchar (200) );
+DELIMITER //
 
- 
+CREATE PROCEDURE GetPatientAppointments(IN p_patient_id INT)
+BEGIN
+
+    DECLARE patient_first_name VARCHAR(255);
+    DECLARE patient_last_name VARCHAR(255);
+    DECLARE doctor_first_name VARCHAR(255);
+    DECLARE doctor_last_name VARCHAR(255);
+   
+    IF EXISTS (SELECT 1 FROM patients WHERE patient_id = p_patient_id) THEN
+       
+        SELECT first_name, last_name INTO patient_first_name, patient_last_name
+        FROM patients
+        WHERE patient_id = p_patient_id;
+
+        SELECT 'Patient Details:', 'Name:', patient_first_name, patient_last_name;
+
+        SELECT appointments.appointment_id,
+               doctors.first_name AS doctor_first_name,
+               doctors.last_name AS doctor_last_name,
+               appointments.task_description,
+               appointments.appointment_date
+        FROM appointments
+        
+        INNER JOIN doctors ON appointments.doctor_id = doctors.doctor_id
+        WHERE appointments.patient_id = p_patient_id;
+        
+    ELSE
+        SELECT 'Patient not found';
+    END IF;
+
+END //
+
+DELIMITER ;
+
+
+CALL GetPatientAppointments(1);
+
