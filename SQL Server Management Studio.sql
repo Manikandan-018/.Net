9922f580b02b19ded2d18499f4dd36e558968532
Manikandan-018 <150133480+Manikandan-018@users.noreply.github.com>
2023-11-29 18:29:42 +0530

    Add files via upload

--- SQL Server Management Studio.sql
+++ SQL Server Management Studio.sql
@@ -1,196 +1,149 @@
 use logicfirst;
 
--- Create tables for Hospital Management System
-
-CREATE TABLE patients (
-    patient_id INT PRIMARY KEY,
-    first_name VARCHAR(15),
-    last_name VARCHAR(10),
-    date_of_birth DATE,
-    gender VARCHAR(10),
-    contact_number VARCHAR(15),
-    address VARCHAR(25)
+-- Create the branches table
+CREATE TABLE branches (
+    branch_id INT PRIMARY KEY,
+    branch_name VARCHAR(25),
+    location VARCHAR(20)
 );
 
+INSERT INTO branches (branch_id, branch_name, location) VALUES
+(1, 'Head Office', 'Chennai'),
+(2, 'Tech', 'Maharastra'),
+(3, 'solutions', 'Rajasthan');
 
-INSERT INTO patients (patient_id, first_name, last_name, date_of_birth, gender, contact_number, address)
-VALUES
-(1, 'John', 'Doe', '2006-05-15', 'Male', '1234567890', '123 Main St'),
-(2, 'Jane', 'Smith', '2011-08-20', 'Female', '9876543210', '456 Oak St');
+select * from branches;
+
+drop table branches;
 
 
-SELECT * FROM patients;
-
-
-CREATE TABLE doctors (
-    doctor_id INT PRIMARY KEY,
+-- Create the employees table
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
     first_name VARCHAR(10),
     last_name VARCHAR(15),
-    specialization VARCHAR(20),
-    contact_number VARCHAR(10),
-    address VARCHAR(28)
+    position VARCHAR(20),
+    branch_id INT,
+    CONSTRAINT fk_branch_id FOREIGN KEY (branch_id) REFERENCES branches(branch_id)
 );
 
 
-INSERT INTO doctors (doctor_id, first_name, last_name, specialization, contact_number, address)
-VALUES
-(1, 'Dr. Michael', 'Johnson', 'Cardiologist', '5551112233', '789 Elm St'),
-(2, 'Dr. Emily', 'Williams', 'Pediatrician', '5554445566', '101 Pine St');
+-- Insert sample data into the employees table
+INSERT INTO employees (employee_id, first_name, last_name, position, branch_id) VALUES
+(1, 'John', 'Doe', 'Manager', 1),
+(2, 'williams', 'Smith', 'Sales Representative', 2),
+(3, 'Robert', 'Johnson', 'Accountant', 3);
 
-SELECT * FROM doctors;
+select * from employees;
+
+drop table employees;
 
 
-CREATE TABLE appointments (
-    appointment_id INT PRIMARY KEY,
-    patient_id INT,
-    doctor_id INT,
-    appointment_date DATETIME,
-    PRIMARY KEY (patient_id, appointment_date),
-    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
-    FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id)
+CREATE TABLE salary (
+    salary_id INT PRIMARY KEY,
+    employee_id INT,
+    salary DECIMAL(10, 2),
+    effective_date DATE,
+    CONSTRAINT fk_employee_id FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
 );
 
 
-INSERT INTO appointments (appointment_id, patient_id, doctor_id, task_description, appointment_date)
-VALUES
-(1, 1, 1, 'Consultation', '2023-04-10 10:00:00');
+INSERT INTO salary (employee_id, salary_id, salary, effective_date) VALUES
+(1, 1, 80000.00, '2023-08-26'),
+(2, 2, 30000.00, '2023-08-29'),
+(3, 3, 50000.00, '2023-08-28');
+
+select * from salary;
+
+drop table salary;
 
 
-SELECT * FROM appointments;
-
-
-CREATE TABLE medical_records (
-    record_id INT PRIMARY KEY,
-    patient_id INT,
-    doctor_id INT,
-    appointment_id INT,
-    diagnosis TEXT,
-    prescription TEXT,
-    test_results TEXT,
-    PRIMARY KEY (patient_id, appointment_id),
-    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
-    FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id),
-    FOREIGN KEY (appointment_id) REFERENCES appointments(appointment_id)
-);
-
-
-SELECT * FROM medical_records;
+-- Query 1: Retrieve all employees with their branch and salary information using INNER JOIN
+SELECT employees.employee_id, employees.first_name, employees.last_name,
+       employees.position, branches.branch_name, branches.location,
+       salary.salary, salary.effective_date
+FROM employees
+INNER JOIN branches ON employees.branch_id = branches.branch_id
+INNER JOIN salary ON employees.employee_id = salary.employee_id;
 
 
 
-CREATE TABLE tasks (
-    task_id INT PRIMARY KEY,
-    task_description VARCHAR(25),
-    task_status VARCHAR(50), 
-    assigned_doctor_id INT, 
-    assigned_patient_id INT,
-    assigned_date DATETIME,
-    FOREIGN KEY (assigned_doctor_id) REFERENCES doctors(doctor_id),
-    FOREIGN KEY (assigned_patient_id) REFERENCES patients(patient_id)
-);
-
-INSERT INTO tasks (task_id, task_description, task_status, assigned_doctor_id, assigned_patient_id, assigned_date)
-VALUES
-(1, 'Blood Test', 'Pending', 1, 1, '2023-04-15 14:00:00');
+-- Query 2: Retrieve all branches and their employee and salary information using LEFT JOIN
+SELECT branches.branch_id, branches.branch_name, branches.location,
+       employees.first_name, employees.last_name, employees.position,
+       salary.salary, salary.effective_date
+FROM branches
+LEFT JOIN employees ON branches.branch_id = employees.branch_id
+LEFT JOIN salary ON employees.employee_id = salary.employee_id;
 
 
 
-DELIMITER //
-
-CREATE PROCEDURE GetPatientAppointments(IN p_patient_id INT)
-BEGIN
-  
-    DECLARE patient_first_name VARCHAR(255);
-    DECLARE patient_last_name VARCHAR(255);
-    DECLARE doctor_first_name VARCHAR(255);
-    DECLARE doctor_last_name VARCHAR(255);
-    
-
-    SELECT first_name, last_name INTO patient_first_name, patient_last_name
-    FROM patients
-    WHERE patient_id = p_patient_id;
-
-    SELECT 'Patient Details:', 'Name:', patient_first_name, patient_last_name;
-
-    SELECT appointments.appointment_id,
-           doctors.first_name AS doctor_first_name,
-           doctors.last_name AS doctor_last_name,
-           appointments.task_description,
-           appointments.appointment_date
-    FROM appointments
-    INNER JOIN doctors ON appointments.doctor_id = doctors.doctor_id
-    WHERE appointments.patient_id = p_patient_id;
-
-END //
-
-DELIMITER ;
+-- Query 3: Retrieve the total number of employees in each branch with their average salary
+SELECT branches.branch_id, branches.branch_name, COUNT(employees.employee_id) AS total_employees,
+       AVG(salary.salary) AS average_salary
+FROM branches
+LEFT JOIN employees ON branches.branch_id = employees.branch_id
+LEFT JOIN salary ON employees.employee_id = salary.employee_id
+GROUP BY branches.branch_id, branches.branch_name;
 
 
 
-SELECT * FROM tasks;
-
-drop table patient;
-
-drop table doctors;
-
-
-
-delimiter $$
-create procedure employeecnt(
-in jdesc varchar(10)
-)
-begin
-declare total int default 0;
-select count(emp_id)
-into total
-from employee
-where job_desc=jdesc;
-select total;
-end$$
-delimiter ;
-
-call employeecnt('ADMIN');
-
-call employeecnt('CEO');
+-- Query 1: Retrieve all employees with their branch information using INNER JOIN
+SELECT employees.employee_id, employees.first_name, employees.last_name,
+       employees.position, branches.branch_name, branches.location
+FROM employees
+INNER JOIN branches ON employees.branch_id = branches.branch_id;
 
 
-
-DELIMITER //
-
-CREATE PROCEDURE GetPatientAppointments(IN p_patient_id INT)
-BEGIN
-
-    DECLARE patient_first_name VARCHAR(255);
-    DECLARE patient_last_name VARCHAR(255);
-    DECLARE doctor_first_name VARCHAR(255);
-    DECLARE doctor_last_name VARCHAR(255);
-   
-    IF EXISTS (SELECT 1 FROM patients WHERE patient_id = p_patient_id) THEN
-       
-        SELECT first_name, last_name INTO patient_first_name, patient_last_name
-        FROM patients
-        WHERE patient_id = p_patient_id;
-
-        SELECT 'Patient Details:', 'Name:', patient_first_name, patient_last_name;
-
-        SELECT appointments.appointment_id,
-               doctors.first_name AS doctor_first_name,
-               doctors.last_name AS doctor_last_name,
-               appointments.task_description,
-               appointments.appointment_date
-        FROM appointments
-        
-        INNER JOIN doctors ON appointments.doctor_id = doctors.doctor_id
-        WHERE appointments.patient_id = p_patient_id;
-        
-    ELSE
-        SELECT 'Patient not found';
-    END IF;
-
-END //
-
-DELIMITER ;
+-- Query 2: Retrieve all branches and their employee information using LEFT JOIN
+SELECT branches.branch_id, branches.branch_name, branches.location,
+       employees.first_name, employees.last_name, employees.position
+FROM branches
+LEFT JOIN employees ON branches.branch_id = employees.branch_id;
 
 
-CALL GetPatientAppointments(1);
+-- Query 3: Retrieve the total number of employees in each branch
+SELECT branches.branch_id, branches.branch_name, COUNT(employees.employee_id) AS total_employees
+FROM branches
+LEFT JOIN employees ON branches.branch_id = employees.branch_id
+GROUP BY branches.branch_id, branches.branch_name;
+
+
+-- Query 4: Update the position of an employee
+UPDATE employees
+SET position = 'Senior Sales Representative'
+WHERE employee_id = 2;
+
+
+-- Query 5: Delete an employee
+DELETE FROM employees
+WHERE employee_id = 3;
+
+
+-- Query 6: Retrieve the highest and lowest salary positions in each branch
+SELECT branches.branch_id, branches.branch_name,
+       MAX(employees.position) AS highest_position,
+       MIN(employees.position) AS lowest_position
+FROM branches
+LEFT JOIN employees ON branches.branch_id = employees.branch_id
+GROUP BY branches.branch_id, branches.branch_name;
+
+-- Query 7: Retrieve employees whose last names start with 'D'
+SELECT * FROM employees
+WHERE last_name LIKE 'D%';
+
+-- Query 8: Retrieve the average salary for each position
+SELECT position, AVG(salary) AS average_salary
+FROM employees
+GROUP BY position;
+
+-- Query 9: Increase the salary of all employees by 10%
+UPDATE employees
+SET salary = salary * 1.1;
+
+-- Query 10: Retrieve the total number of employees for each branch, including branches with no employees
+SELECT branches.branch_id, branches.branch_name, COUNT(employees.employee_id) AS total_employees
+FROM branches
+LEFT JOIN employees ON branches.branch_id = employees.branch_id
+GROUP BY branches.branch_id, branches.branch_name;
 
